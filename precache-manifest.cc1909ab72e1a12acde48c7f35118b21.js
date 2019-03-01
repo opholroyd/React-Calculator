02f83157e4aaafc81d40a04632d0ff3d87409f7e
Administrator <administrator@CN-8.lan>
2019-03-01 12:16:06 +0000

    Updates

---
+++ precache-manifest.cc1909ab72e1a12acde48c7f35118b21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08a57c4877ae33407fbe",
+    "url": "/React-Calculator/static/js/runtime~main.08a57c48.js"
+  },
+  {
+    "revision": "762923080007dc33e9eb",
+    "url": "/React-Calculator/static/js/main.76292308.chunk.js"
+  },
+  {
+    "revision": "a6b8328d9e7f64ae5848",
+    "url": "/React-Calculator/static/js/2.a6b8328d.chunk.js"
+  },
+  {
+    "revision": "762923080007dc33e9eb",
+    "url": "/React-Calculator/static/css/main.d21eec27.chunk.css"
+  },
+  {
+    "revision": "751d70dd723aac55618c3ec7561ebb07",
+    "url": "/React-Calculator/index.html"
+  }
+];
